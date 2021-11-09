d21488acdad06d8389d5b87050fb6f30bdb9bf10
deniseman <pop.denis1902@gmail.com>
2021-11-10 01:41:56 +0200

    Image fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -38,7 +38,7 @@ class RecipeView extends View {
     <figure class="recipe__fig">
           <img src="${this._data.image}" alt="${
       this._data.title
-    } class="recipe__img" />
+    }" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${this._data.title}</span>
           </h1>
